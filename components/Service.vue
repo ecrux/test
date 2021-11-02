692d4602c68216077366693d3478edeaf40ba805
Edwar Cruz <estebancruz373@gmail.com>
2021-11-02 17:10:40 -0500

    Second commit

--- components/Service.vue
+++ components/Service.vue
@@ -1,3 +1,26 @@
 <template>
-    
+    <div class="d-flex justify-content-center mt-5">
+        
+        <div class="p-2 m-4">
+            <b-img src="https://www.nexxtsolutions.com/media/Icono_Streaming-4k(0).png" alt="img icon streaming" title="Streaming 4K"></b-img>
+            <p class="p-3">Streaming 4K</p>
+        </div>
+        <div class="p-2 m-4">
+            <b-img src="https://www.nexxtsolutions.com/media/Icono_Home_Automation(0).png" alt="img icon Home" title="ome Automation"></b-img>
+            <p class="p-3">Home Automation</p>
+        </div>
+        <div class="p-2 m-4">
+            <b-img src="https://www.nexxtsolutions.com/media/Icono_File-Transfer(0).png" alt="img icon file transfer" title="File Transfer"></b-img>
+            <p class="p-3">File Transfer</p>
+        </div>
+        <div class="p-2 m-4">
+            <b-img src="https://www.nexxtsolutions.com/media/Icono_Music-Streaming(0).png" alt="img icon Music Stream" title="Music Streaming"></b-img>
+            <p class="p-3">Music Streaming</p>
+        </div>
+        <div class="p-2 m-4">
+            <b-img src="https://www.nexxtsolutions.com/media/Icono_Videovigilancia(0).png" alt="img icon Video surve" title="Video Surveillance"></b-img>
+            <p class="p-3">Video Surveillance</p>
+        </div>
+        
+    </div>
 </template>
